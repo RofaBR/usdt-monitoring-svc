4feeec5f06f0da3649d5da09ec68928bdcd0ee5b
RofaBR <rostislavbrigine@gmail.com>
2024-08-13 20:49:31 +0300

    migration for creating table

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,3 +1,13 @@
 -- +migrate Up
 
+CREATE TABLE usdt_transfers (
+    id SERIAL PRIMARY KEY,
+    transaction_hash VARCHAR(66) NOT NULL,
+    block_number BIGINT NOT NULL,
+    from_address VARCHAR(42) NOT NULL,
+    to_address VARCHAR(42) NOT NULL,
+    amount NUMERIC(78, 0) NOT NULL,
+    timestamp TIMESTAMP NOT NULL
+);
+
 -- +migrate Down
