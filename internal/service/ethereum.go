e04a8207143363e4e6bf4c5bbf8c306e9dea2916
RofaBR <rostislavbrigine@gmail.com>
2024-08-26 23:37:37 +0300

    error correction

--- internal/service/ethereum.go
+++ internal/service/ethereum.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/RofaBR/usdt-monitoring-svc/internal/config"
 	usdt "github.com/RofaBR/usdt-monitoring-svc/internal/ethereum"
+	"github.com/RofaBR/usdt-monitoring-svc/internal/storage"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -13,7 +14,7 @@ import (
 
 const (
 	USDTContractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-	BlockRange          = 1000
+	BlockRange          = 100
 )
 
 func (s *service) connectToEthereum(cfg config.Config) *ethclient.Client {
@@ -45,27 +46,52 @@ func (s *service) GetTransferEvents(cfg config.Config) {
 		return
 	}
 
-	startBlock := big.NewInt(0)
-	endBlock := new(big.Int).Add(startBlock, big.NewInt(BlockRange))
-
-	query := ethereum.FilterQuery{
-		FromBlock: startBlock,
-		ToBlock:   endBlock,
-		Addresses: []common.Address{contractAddress},
-	}
-
-	logs, err := client.FilterLogs(context.Background(), query)
+	header, err := client.HeaderByNumber(context.Background(), nil)
 	if err != nil {
-		s.log.WithError(err).Error("Failed to filter logs")
+		s.log.WithError(err).Error("Failed to get latest block header")
 		return
 	}
+	currentBlock := header.Number
+	startBlock := new(big.Int).Sub(currentBlock, big.NewInt(BlockRange))
 
-	for _, vLog := range logs {
-		transferEvent, err := usdtContract.ParseTransfer(vLog)
-		if err != nil {
-			s.log.WithError(err).Error("Failed to parse log")
-			continue
+	for startBlock.Cmp(currentBlock) < 0 {
+		endBlock := new(big.Int).Add(startBlock, big.NewInt(BlockRange))
+		if endBlock.Cmp(currentBlock) > 0 {
+			endBlock = currentBlock
 		}
-		s.log.Infof("Transfer event: From: %s, To: %s, Tokens: %s", transferEvent.From.Hex(), transferEvent.To.Hex(), transferEvent.Value.String())
+
+		query := ethereum.FilterQuery{
+			FromBlock: startBlock,
+			ToBlock:   endBlock,
+			Addresses: []common.Address{contractAddress},
+		}
+
+		logs, err := client.FilterLogs(context.Background(), query)
+		if err != nil {
+			s.log.WithError(err).Errorf("Failed to filter logs for block range [%s, %s]", startBlock.String(), endBlock.String())
+			break
+		}
+
+		for _, vLog := range logs {
+			transferEvent, err := usdtContract.ParseTransfer(vLog)
+			if err != nil {
+				s.log.WithError(err).Error("Failed to parse log")
+				continue
+			}
+
+			s.log.Infof("Transfer event: From: %s, To: %s, Tokens: %s", transferEvent.From.Hex(), transferEvent.To.Hex(), transferEvent.Value.String())
+
+			err = s.storage.SaveTransferEvent(context.Background(), storage.TransferEvent{
+				From:   transferEvent.From.Hex(),
+				To:     transferEvent.To.Hex(),
+				Amount: transferEvent.Value.String(),
+				TxHash: vLog.TxHash.Hex(),
+			})
+			if err != nil {
+				s.log.WithError(err).Error("Failed to save transfer event")
+			}
+		}
+
+		startBlock = new(big.Int).Add(endBlock, big.NewInt(1))
 	}
 }
